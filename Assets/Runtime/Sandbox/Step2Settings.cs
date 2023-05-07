98d44cb23137de462b9ed3cb3e95f8e48c5d9472
Tobias S <>
2023-05-07 11:55:16 +0800

    add Step2Settings validation to ensure always valid map chunks

--- Assets/Runtime/Sandbox/Step2Settings.cs
+++ Assets/Runtime/Sandbox/Step2Settings.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "MapGen/Step2")]
 public class Step2Settings : MapGenStepSettings
 {
-    public Vector2Int   mapChunkDimensions         = new Vector2Int(20, 20);
+    public Vector2Int   mapChunkDimensions          = new Vector2Int(20, 20);
 
     public int          mapChunkWallSize            = 1;
 
@@ -13,4 +13,17 @@ public class Step2Settings : MapGenStepSettings
 
     [Range(0, 1)]
     public float        walkablePathDisplacement    = 0.0f;
+
+
+    public void OnValidate()
+    {
+        this.mapChunkDimensions.x   = Mathf.Max(3, this.mapChunkDimensions.x);
+        this.mapChunkDimensions.y   = Mathf.Max(3, this.mapChunkDimensions.y);
+
+        var minSideSize             = Mathf.Min(this.mapChunkDimensions.x, this.mapChunkDimensions.y);
+        var maxWallSize             = Mathf.FloorToInt((float)minSideSize / 2.0f);
+        this.mapChunkWallSize       = Mathf.Clamp(this.mapChunkWallSize, 0, maxWallSize);
+
+        this.walkablePathThickness  = Mathf.Clamp(walkablePathThickness, 0, minSideSize - (2 * this.mapChunkWallSize));
+    }
 }
