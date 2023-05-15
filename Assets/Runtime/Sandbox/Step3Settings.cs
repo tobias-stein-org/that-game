e60328289339fcdc207c8084c6e3cdf1d3a794e5
Tobias S <>
2023-05-15 11:46:44 +0800

    add similarity threshold value for module comparision

--- Assets/Runtime/Sandbox/Step3Settings.cs
+++ Assets/Runtime/Sandbox/Step3Settings.cs
@@ -7,6 +7,8 @@ public class Step3Settings : MapGenStepSettings
 {
     public int              numAttemptsToSolveMapChunk = 3;
 
+    public float            similarityThreshold = 5.0f;
+
     public void OnValidate()
     {
         this.numAttemptsToSolveMapChunk = Mathf.Max(1, this.numAttemptsToSolveMapChunk);
