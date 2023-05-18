1210b23878176b70436e4d041360c3cc97048659
Tobias S <>
2023-05-18 11:24:22 +0800

    only print debug text if camera is close to plane

--- Assets/Editor/MapGen_LayoutEditor.cs
+++ Assets/Editor/MapGen_LayoutEditor.cs
@@ -9,33 +9,45 @@ public class MapGen_LayoutEditor : Editor
     void OnSceneGUI()
     {
         var target = this.target as MapGen_Layout;
-        if(target != null && target.done)
+        if(target != null && target.executor != null && !target.executor.isBusy)
         {
             var data = target.executor.data;
+            float camDistance = Mathf.Abs(SceneView.lastActiveSceneView.camera.transform.position.z);
 
             for(int c = 0; c < data.pathSteps.Length + 1; c++)
             {
                 var chunkInfo = data.getChunkInfo(c);
                 var chunkData = data.getChunkData(c);
 
-                var restoreColor = GUI.color;
-                GUI.color = Color.red;
-                Handles.Label(new Vector3(chunkInfo.bounds.x + 0.25f, chunkInfo.bounds.y + 0.25f, 0) * 0.16f, $"{c}");
-                GUI.color = restoreColor;
+                this.drawMapChunkId(chunkInfo, c);
 
-                for(int y = 0; y < chunkInfo.bounds.height; y++)
-                for(int x = 0; x < chunkInfo.bounds.width; x++)
+                if(camDistance < 1.0f)
+                {
+                    for(int y = 0; y<chunkInfo.bounds.height; y++)
+                    for(int x = 0; x<chunkInfo.bounds.width; x++)
                     {
-                    int i           = (y * chunkInfo.bounds.width) + x;
+                        int i = (y * chunkInfo.bounds.width) + x;
 
-                    var cType       = chunkData[i].constructionType;
-                    var tilePos     = new Vector3(chunkInfo.bounds.x + x + 0.5f, chunkInfo.bounds.y + y + 0.5f, 0) * 0.16f;
+                        var cType = chunkData[i].constructionType;
 
-                    Handles.Label(tilePos, $"{cType.ToString()[0]}");
+                        if(chunkData[i].obstrModuleId != -1)
+                            Handles.Label(new Vector3(chunkInfo.bounds.x + x + 0.1f, -(chunkInfo.bounds.y + y) + 0.66f, 0) * 0.16f, $"{target.generatorSettings.modules[chunkData[i].obstrModuleId].name} [T: {target.generatorSettings.modules[chunkData[i].obstrModuleId].constructionType.ToString()[0]}, ID: {chunkData[i].obstrModuleId}]");
+
+                        Handles.Label(new Vector3(chunkInfo.bounds.x + x + 0.1f, -(chunkInfo.bounds.y + y) + 0.33f, 0) * 0.16f, $"({cType.ToString()[0]})");
+                    }
                 }
             }
         }
     }
+
+    private void drawMapChunkId(in MapChunkInfo chunkInfo, int chunkId)
+    {
+        var restoreColor = GUI.color;
+        GUI.color = Color.red;
+        Handles.Label(new Vector3(chunkInfo.bounds.x + 0.25f, -chunkInfo.bounds.y + 0.25f, 0) * 0.16f, $"{chunkId}");
+        GUI.color = restoreColor;
+    }
+
 
     public override void OnInspectorGUI()
     {
