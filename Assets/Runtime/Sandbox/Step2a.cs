94c1ddc8d88a9e8fd812a320c6b72a7586124f34
Tobias S <>
2023-05-21 08:14:54 +0800

    improve random walk

--- Assets/Runtime/Sandbox/Step2a.cs
+++ Assets/Runtime/Sandbox/Step2a.cs
@@ -15,144 +15,111 @@ public class Step2a : MapGenStep<Step2aSettings>
             var chunkInfo   = context.data.getChunkInfo(i);
             var chunkData   = context.data.getChunkData(i);
 
-            Vector2Int from = context.data.getChunkInfo(0).bounds.size / 2;
+            Vector2Int start = context.data.getChunkInfo(0).bounds.size / 2;
             if(i > 0)
             {
                 var lastChunkInfo = context.data.getChunkInfo(i - 1);
                 var offset = lastChunkInfo.bounds.position - chunkInfo.bounds.position;
                 offset += new Vector2Int(offset.x != 0 ? -(int)Mathf.Sign(offset.x) : 0, offset.y != 0 ? -(int)Mathf.Sign(offset.y) : 0);
 
-                from = lastChunkInfo.pathExit + offset;
+                start = lastChunkInfo.pathExit + offset;
             }
 
-             var tileId = (from.y * chunkInfo.bounds.width) + from.x;
-             var tileData = chunkData[tileId];
+            var tileId = (start.y * chunkInfo.bounds.width) + start.x;
+            var tileData = chunkData[tileId];
 
-             tileData.constructionType = TileConstructionType.Walkable;
-             chunkData[tileId] = tileData;
+            tileData.constructionType = TileConstructionType.Walkable;
+            chunkData[tileId] = tileData;
 
-            Vector2Int to   = chunkInfo.pathExit;
+            var exit                            = chunkInfo.pathExit;
 
-            var currentPos = from;
-            while(currentPos != to)
+            var camefrom                        = i > 0                                 ? -context.data.pathSteps[i - 1] : Vector2Int.zero;
+            var goTo                            = i < context.data.pathSteps.Length     ?  context.data.pathSteps[i]     : Vector2Int.zero;
+
+
+
+            for(int s = 0; s < chunkInfo.wallSize + 1; s++)
             {
-                var options = this.walkOptions(currentPos, chunkInfo, chunkData);
-                currentPos = options[context.random.Next(0, options.Count)];
-
-                tileId = (currentPos.y * chunkInfo.bounds.width) + currentPos.x;
+                tileId                      = (start.y * chunkInfo.bounds.width) + start.x;
                 tileData = chunkData[tileId];
 
                 tileData.constructionType = TileConstructionType.Walkable;
                 chunkData[tileId] = tileData;
+
+                start -= camefrom;
             }
 
-            var camefrom = i > 0 ? -context.data.pathSteps[i - 1]  : Vector2Int.zero;
-
-            if(camefrom == Vector2Int.up)
+            for(int s = 0; s < chunkInfo.wallSize + 1; s++)
             {
-                for(int x = chunkInfo.wallSize; x < chunkInfo.bounds.width - chunkInfo.wallSize; x++)
-                {
-                    if(x == from.x) { continue; }
+                tileId                      = (exit.y * chunkInfo.bounds.width) + exit.x;
+                tileData = chunkData[tileId];
 
-                    tileId = ((chunkInfo.bounds.height - 1) * chunkInfo.bounds.width) + x;
-                    tileData = chunkData[tileId];
+                tileData.constructionType = TileConstructionType.Walkable;
+                chunkData[tileId] = tileData;
 
-                    if(tileData.constructionType == TileConstructionType.Walkable)
-                    {
-                        tileData.constructionType = TileConstructionType.Undefined;
-                        chunkData[tileId] = tileData;
-                    }
-                }
+                exit -= goTo;
             }
-            else if(camefrom == Vector2Int.down)
-            {
-                for(int x = chunkInfo.wallSize; x < chunkInfo.bounds.width - chunkInfo.wallSize; x++)
-                {
-                    if(x == from.x) { continue; }
 
-                    tileId = x;
+            // do random walk inside the map chunk with a 1 tile margin to the borders
+            { 
+                RectInt                 area    = new RectInt
+                {
+                    x                           = chunkInfo.wallSize        + 1,
+                    y                           = chunkInfo.wallSize        + 1,
+                    width                       = chunkInfo.bounds.width    - chunkInfo.wallSize - 1,
+                    height                      = chunkInfo.bounds.height   - chunkInfo.wallSize - 1
+                };
+                NativeList<Vector2Int>  options = new NativeList<Vector2Int>(4, Allocator.Persistent);
+
+                var currentPos                  = start;
+                tileId                          = (currentPos.y * chunkInfo.bounds.width) + currentPos.x;
+                tileData                        = chunkData[tileId];
+
+                tileData.constructionType       = TileConstructionType.Walkable;
+                chunkData[tileId]               = tileData;
+
+                while(currentPos != exit)
+                {
+                    this.walkOptions(ref options, area, currentPos);
+                    currentPos = options[context.random.Next(0, options.Length)];
+
+                    tileId = (currentPos.y * chunkInfo.bounds.width) + currentPos.x;
                     tileData = chunkData[tileId];
 
-                    if(tileData.constructionType == TileConstructionType.Walkable)
-                    {
-                        tileData.constructionType = TileConstructionType.Undefined;
-                        chunkData[tileId] = tileData;
-                    }
+                    tileData.constructionType = TileConstructionType.Walkable;
+                    chunkData[tileId] = tileData;
                 }
-            }
-            else if(camefrom == Vector2Int.left)
-            {
-                for(int y = chunkInfo.wallSize; y < chunkInfo.bounds.height - chunkInfo.wallSize; y++)
-                {
-                    if(y == from.y) { continue; }
 
-                    tileId = (y * chunkInfo.bounds.width);
-                    tileData = chunkData[tileId];
-
-                    if(tileData.constructionType == TileConstructionType.Walkable)
-                    {
-                        tileData.constructionType = TileConstructionType.Undefined;
-                        chunkData[tileId] = tileData;
-                    }
-                }
-            }
-            else if(camefrom == Vector2Int.right)
-            {
-                for(int y = chunkInfo.wallSize; y < chunkInfo.bounds.height - chunkInfo.wallSize; y++)
-                {
-                    if(y == from.y) { continue; }
-
-                    tileId = (y * chunkInfo.bounds.width) + chunkInfo.bounds.width - 1;
-                    tileData = chunkData[tileId];
-
-                    if(tileData.constructionType == TileConstructionType.Walkable)
-                    {
-                        tileData.constructionType = TileConstructionType.Undefined;
-                        chunkData[tileId] = tileData;
-                    }
-                }
+                options.Dispose();
             }
         }
 
         yield return new MapGenStepState {};
     }
 
-    private List<Vector2Int> walkOptions(in Vector2Int current, in MapChunkInfo info, in NativeSlice<MapChunkData> data)
+    private void walkOptions(ref NativeList<Vector2Int> options, in RectInt area, in Vector2Int current)
     {
-        var options = new List<Vector2Int>(4);
+        options.Clear();
 
         // walk left
         var walkLeft = current + Vector2Int.left;
-        if(walkLeft.x >= 0 && data[(walkLeft.y * info.bounds.width) + walkLeft.x].constructionType != TileConstructionType.Obstructed)
-            options.Add(walkLeft);
+        if(walkLeft.x >= area.x)
+            options.AddNoResize(walkLeft);
 
         // walk right
         var walkRight = current + Vector2Int.right;
-        if(walkRight.x < info.bounds.width && data[(walkRight.y * info.bounds.width) + walkRight.x].constructionType != TileConstructionType.Obstructed)
-            options.Add(walkRight);
+        if(walkRight.x < area.width)
+            options.AddNoResize(walkRight);
 
         // walk top
         var walkTop = current + Vector2Int.up;
-        if(walkTop.y < info.bounds.height && data[(walkTop.y * info.bounds.width) + walkTop.x].constructionType != TileConstructionType.Obstructed)
-            options.Add(walkTop);
+        if(walkTop.y < area.height)
+            options.AddNoResize(walkTop);
 
         // walk bottom
         var walkBottom = current + Vector2Int.down;
-        if(walkBottom.y >= 0 && data[(walkBottom.y * info.bounds.width) + walkBottom.x].constructionType != TileConstructionType.Obstructed)
-            options.Add(walkBottom);
-
-        return options;
-    }
-
-    static List<List<T>> partition<T>(List<T> source, int n)
-    {
-        if(n == 0) { return new List<List<T>> { source }; }
-
-        return source
-            .Select((num, index) => new { Number = num, Index = index / n })
-            .GroupBy(x => x.Index)
-            .Select(group => group.Select(x => x.Number).ToList())
-            .ToList();
+        if(walkBottom.y >= area.y)
+            options.AddNoResize(walkBottom);
     }
 
     public override void initialize(MapGeneratorSettings context)
