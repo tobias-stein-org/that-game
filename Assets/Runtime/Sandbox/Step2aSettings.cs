72af9497dd40b356d149cd7abb89628572474ca6
Tobias S <>
2023-05-21 13:22:54 +0800

    remove unused setting

--- Assets/Runtime/Sandbox/Step2aSettings.cs
+++ Assets/Runtime/Sandbox/Step2aSettings.cs
@@ -5,6 +5,4 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "MapGen/Step2a")]
 public class Step2aSettings : MapGenStepSettings
 {
-    [Range(0.0f, 1.0f)]
-    public float pathConvolution = 0.8f;
 }
