7203010176d9192278cfd2eec9254821f70fc64f
Tobias S <>
2023-06-02 16:38:10 +0800

    merge level generator and render into single MonoBehaviour

--- Assets/Runtime/tg/Modules/Level/Events.cs
+++ Assets/Runtime/tg/Modules/Level/Events.cs
@@ -6,22 +6,25 @@ namespace tg.level.events
     using tg.level.generator;
 
     /// <summary>
-    /// LevelRender will listen to these events and repaint the entire tilemap with the current data provided.
-    /// </summary>
-    public struct RequestRenderLevelEvent : IEvent
-    {
-        public LevelData    levelData;
-        public List<Module> modules;
-    }
-
-    /// <summary>
     /// Processed by the LevelGeneratorSystem to create a new random level based on the provided settings.
     /// </summary>
-    public struct RequestNewLevel : IEvent
+    public struct RequestNewLevelEvent : IEvent
     {
         /// <summary>
         /// If this field is null, the LevelGeneratorSystem will use the previous provided settings.
         /// </summary>
         public GeneratorSettings settings;
+    }
+
+
+    public struct LevelGenerationStartedEvent : IEvent {}
+    public struct LevelGenerationAbortedEvent : IEvent {}
+
+    /// <summary>
+    /// Event fired by LevelGenerator once the level is fully created.
+    /// </summary>
+    public struct NewLevelGeneratedEvent : IEvent
+    {
+        public LevelData    levelData;
     }
 }
---
+++ Assets/Runtime/tg/Modules/Level/Level.cs
@@ -0,0 +1,159 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+using Unity.Entities;
+
+namespace tg.level
+{
+    using tg.events;
+    using tg.level.generator;
+    using tg.level.events;
+
+    /// <summary>
+    /// The Level behaviour acts as a manager to generate new levels and repaint the tilemap layers.
+    /// </summary>
+    public class Level : MonoBehaviour, IEventListener<Level>
+    {
+        public GeneratorSettings                        initialSettings;
+
+        private Generator                               generator;
+        private Coroutine                               executor;
+
+        private Dictionary<LevelData.Layer, Tilemap>    layers;
+
+        private EntityManager                           entityManager;
+
+
+        void Awake()
+        {
+            this.setupTilemap();
+            this.entityManager  = World.DefaultGameObjectInjectionWorld.EntityManager;
+        }
+
+        void Start()
+        {
+            this.initializeNewGenerator(this.initialSettings);
+            this.startGenerator();
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space)) { this.startGenerator(); }
+
+        }
+
+        void OnEnable()
+        {
+            EventQueue.subscribe(this);
+        }
+
+        void OnDisable()
+        {
+            EventQueue.unsubscribe(this);
+        }
+
+        private void setupTilemap()
+        {
+            this.layers = new Dictionary<LevelData.Layer, Tilemap>(LevelData.Layer.MAX_LAYERS);
+
+            var gridGO  = new GameObject("Level.Grid");
+            {
+                var grid = gridGO.AddComponent<Grid>();
+
+                foreach(var layer in new[] { LevelData.Layer.Floor, LevelData.Layer.Obstructable })
+                {
+                    var layerGO = new GameObject($"Layer.{(int)layer}");
+                    {
+                        layerGO.transform.SetParent(gridGO.transform);
+                        var renderer = layerGO.AddComponent<TilemapRenderer>();
+                        renderer.sortingOrder = layer;
+
+                        layers.Add(layer, layerGO.GetComponent<Tilemap>());
+                    }
+                }
+            }
+        }
+
+        private void initializeNewGenerator(GeneratorSettings settings)
+        {
+            
+            this.abortCurrentGenerator();
+
+            this.generator = new Generator(settings);
+
+            this.generator.onLevelGeneratorStarted += () => EventQueue.publish(new LevelGenerationStartedEvent {});
+
+            this.generator.onLevelGeneratorFinished += (in LevelData data) =>
+            {
+                // expose LevelData as singleton
+                this.entityManager.SetComponentData(this.entityManager.CreateSingleton<LevelData>(), data);
+
+                EventQueue.publish(new NewLevelGeneratedEvent { levelData = data });
+                this.renderTilemap(in data);
+            };
+        }
+
+        private void abortCurrentGenerator()
+        {
+            if(this.entityManager.CreateEntityQuery(typeof(LevelData)).TryGetSingletonEntity<LevelData>(out Entity levelData)) { this.entityManager.DestroyEntity(levelData); }
+
+            if(this.executor != null)
+            {
+                this.StopCoroutine(this.executor);
+                this.executor = null;
+
+                EventQueue.publish(new LevelGenerationAbortedEvent {});
+            }
+
+            this.generator?.Dispose();
+        }
+
+        private void startGenerator()
+        {
+            this.abortCurrentGenerator();
+            this.executor = this.StartCoroutine(this.generator.execute());
+        }
+
+        private void renderTilemap(in LevelData levelData)
+        {
+            // clear previous tilemap rendering
+            foreach(var (layer, tilemap) in this.layers) { tilemap.ClearAllTiles(); }
+
+            for(int chunkId = 0; chunkId < levelData.numChunks; chunkId++)
+            {
+                var chunk = levelData.getChunk(chunkId);
+                for(int y = 0; y < chunk.bounds.height; y++)
+                for(int x = 0; x < chunk.bounds.width; x++)
+                {
+                    var tileId  = (y * chunk.bounds.width) + x;
+                    var tile    = chunk[tileId];
+                    var tilePos = new Vector3Int(chunk.bounds.x + x, chunk.bounds.y + y, 0);
+                    foreach(var (layer, tilemap) in this.layers)
+                    {
+                        var module = tile[layer].id;
+                        if(module != LevelData.Module.INVALID)
+                        {
+                            tilemap.SetTile(tilePos, this.generator.context.settings.modules[module]);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generate a new random level.
+        /// </summary>
+        /// <param name="e"></param>
+        private void onRequestNewLevel(RequestNewLevelEvent e)
+        {
+            if(e.settings != null)
+            {
+                this.initializeNewGenerator(e.settings);
+            }
+
+            this.startGenerator();
+        }
+    }
+}
