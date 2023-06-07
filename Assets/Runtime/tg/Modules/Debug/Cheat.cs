c078b535b3049a52fd034511ff401ef494c4fd6f
Tobias S <>
2023-06-07 14:00:57 +0800

    add simple cheat system for debugging

---
+++ Assets/Runtime/tg/Modules/Debug/Cheat.cs
@@ -0,0 +1,53 @@
+using Unity.Entities;
+
+using UnityEngine.InputSystem;
+
+namespace tg.debug
+{
+    using tg.events;
+    using tg.application;
+
+    using tg.level.events;
+    using tg.player.events;
+
+    public partial class Cheat : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            this.RequireForUpdate<ApplicationData>();
+        }
+
+        protected override void OnUpdate()
+        {
+            while(SystemAPI.GetSingleton<ApplicationData>().inputActions.LoadingStatus != Unity.Entities.Content.ObjectLoadingStatus.Completed) { return; }
+
+
+            var debugActions = SystemAPI.GetSingleton<ApplicationData>().inputActions.Result.FindActionMap("Debug");
+
+            debugActions.FindAction("quit").performed += onQuit;
+            debugActions.FindAction("spawn_player").performed += onSpawnPlayer;
+            debugActions.FindAction("new_level").performed += onNewLevel;
+
+            this.Enabled = false;
+        }
+
+        private void onNewLevel(InputAction.CallbackContext obj)
+        {
+            EventQueue.publish(new RequestNewLevelEvent {});
+        }
+
+        private void onSpawnPlayer(InputAction.CallbackContext obj)
+        {
+            EventQueue.publish(new SpawnPlayerRequestEvent {});   
+        }
+
+        private void onQuit(InputAction.CallbackContext obj)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
