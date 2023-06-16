d373e0edbe75a87f329c2170e3f8c6d78abe9c9b
Tobias S <>
2023-06-16 20:54:50 +0800

    visualize distance value and target tag

--- Assets/Editor/tg/Modules/Debug/Enemy/SensorVisual.cs
+++ Assets/Editor/tg/Modules/Debug/Enemy/SensorVisual.cs
@@ -5,11 +5,30 @@ namespace tg.editor.debug
 {
     using tg.ai;
     using tg.debug;
+    using tg.application;
+    using static UnityEditor.FilePathAttribute;
 
     [CustomEditor(typeof(EnemyDebugging))]
     public class SensorVisual : Editor
     {
         public new EnemyDebugging target { get { return base.target as EnemyDebugging; } }
+
+        GUIStyle targetLabel;
+        GUIStyle distanceLabel;
+
+        private void Awake()
+        {
+            this.targetLabel = new GUIStyle();
+            {
+                this.targetLabel.normal.textColor = Color.cyan;
+            }
+
+            this.distanceLabel = new GUIStyle();
+            {
+                this.distanceLabel.normal.textColor = Color.white;
+                this.distanceLabel.fontSize = 8;
+            }
+        }
 
         void OnSceneGUI()
         {
@@ -27,10 +46,15 @@ namespace tg.editor.debug
             {
                 var output      = sensor.outputs[i];
                 var target      = new Vector3(output.point.x, output.point.y, 0);
+                var dir         = (target - position).normalized;
+
+                Handles.Label(position + (dir * output.distance * 0.5f), $"{output.distance:.02}", distanceLabel);
 
                 Handles.color   = Color.magenta;
                 Handles.DrawSolidDisc(target, Vector3.forward, 0.1f);
-                Handles.DrawDottedLine(position, target, 1.0f);
+                Handles.DrawLine(position, target, 1.0f);
+
+                Handles.Label(target, tg.application.tags.hash2Name(output.tag), this.targetLabel);
             }
         }
     }
