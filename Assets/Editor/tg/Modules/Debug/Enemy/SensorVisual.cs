61956086ae362b072ec1c505ba3ff6d3e8edd568
Tobias S <>
2023-06-17 11:41:47 +0800

    make visualization configurable, add behaviour context visuals

--- Assets/Editor/tg/Modules/Debug/Enemy/SensorVisual.cs
+++ Assets/Editor/tg/Modules/Debug/Enemy/SensorVisual.cs
@@ -7,6 +7,7 @@ namespace tg.editor.debug
     using tg.debug;
     using tg.application;
     using static UnityEditor.FilePathAttribute;
+    using UnityEngine.UIElements;
 
     [CustomEditor(typeof(EnemyDebugging))]
     public class SensorVisual : Editor
@@ -15,6 +16,9 @@ namespace tg.editor.debug
 
         GUIStyle targetLabel;
         GUIStyle distanceLabel;
+        GUIStyle behaviourWeightLabel;
+
+        Gradient behaviourValueColor;
 
         private void Awake()
         {
@@ -28,33 +32,108 @@ namespace tg.editor.debug
                 this.distanceLabel.normal.textColor = Color.white;
                 this.distanceLabel.fontSize = 8;
             }
+
+            this.behaviourWeightLabel = new GUIStyle();
+            {
+                this.behaviourWeightLabel.normal.textColor = Color.magenta;
+            }
+
+
+            // Create a new gradient
+            this.behaviourValueColor = new Gradient();
+        
+            GradientColorKey[] colorKeys = new GradientColorKey[3];
+            colorKeys[0] = new GradientColorKey(Color.green,    0.0f);
+            colorKeys[1] = new GradientColorKey(Color.white,    0.5f);
+            colorKeys[2] = new GradientColorKey(Color.red,      1.0f);
+        
+            GradientAlphaKey[] alphaKeys = new GradientAlphaKey[3];
+            alphaKeys[0] = new GradientAlphaKey(1.0f, 0.0f);
+            alphaKeys[1] = new GradientAlphaKey(1.0f, 0.5f);
+            alphaKeys[2] = new GradientAlphaKey(1.0f, 1.0f);
+        
+            this.behaviourValueColor.SetKeys(colorKeys, alphaKeys);
+            this.behaviourValueColor.mode = GradientMode.PerceptualBlend;
         }
 
         void OnSceneGUI()
+        {
+            if((this.target.sensorDetails & EnemyDebugging.SensorDetails.Hide) == 0) { this.drawSensor(); }
+            if(this.target.contextBehaviourDetails != EnemyDebugging.ContextBehaviourDetails.Hide) { this.drawBehaviourContext(); }
+        }
+
+        private void drawBehaviourContext()
+        {
+            var context     = this.target.behaviourContextData;
+            var position    = this.target.transform.position;
+            var scale       = 2.0f;
+            
+            if(!context.context.isValid)
+            {
+                Handles.Label(position, $"Behaviour Context not available!", this.behaviourWeightLabel);
+                return;
+            }
+
+            Handles.color = Color.white * 0.5f;
+            Handles.DrawWireDisc(position, Vector3.forward, scale);
+
+            var colors      = new Color[BehaviourContext.segmentDir.Length + 1];
+            var points      = new Vector3[BehaviourContext.segmentDir.Length + 1];
+            var points1     = new Vector3[BehaviourContext.segmentDir.Length + 1];
+
+            int i = 0;
+            for(i = 0; i < BehaviourContext.segmentDir.Length; i++)
+            {
+                var offset  = (Vector3)BehaviourContext.segmentDir[i] * scale;
+
+                Handles.DrawLine(position, position + offset, 1.0f);
+
+                // values are expected to be in the range of [-1; +1]
+                var value   = context.context[i];
+
+                points[i]   = position + (offset * Mathf.Abs(value));
+                points1[i]  = position + (offset * Mathf.Abs(context.context1[i]));
+
+                colors[i]   = this.behaviourValueColor.Evaluate((value + 1.0f) * 0.5f);
+            }
+            points[i]       = points[0];
+            points1[i]      = points1[0];
+            colors[i]       = colors[0];
+
+            Handles.color = Color.white * 0.33f;
+            Handles.DrawAAConvexPolygon(points1);
+            Handles.DrawAAPolyLine(6.0f, colors, points);
+
+            Handles.Label(position, $"W: {context.weight}, B: {context.blend}", this.behaviourWeightLabel);
+        }
+
+        private void drawSensor()
         {
             var position    = this.target.transform.position;
             var sensor      = this.target.sensor;
 
             // draw sensor range
+            if((this.target.sensorDetails & EnemyDebugging.SensorDetails.ShowRange) != 0)
             {
                 Handles.color   = Color.white;
                 Handles.DrawWireDisc(position, Vector3.forward, sensor.desc.senorPerceptionRange, 2.0f);
             }
 
-            // draw percieved objects
-            for(int i = 0; i < sensor.outputs.Length; i++)
+            if((this.target.sensorDetails & EnemyDebugging.SensorDetails.ShowOutput) != 0)
             {
-                var output      = sensor.outputs[i];
-                var target      = new Vector3(output.point.x, output.point.y, 0);
-                var dir         = (target - position).normalized;
+                // draw percieved objects
+                for(int i = 0; i < sensor.outputs.Length; i++)
+                {
+                    var output      = sensor.outputs[i];
+                    var target      = new Vector3(output.point.x, output.point.y, 0);
+                    var dir         = (target - position).normalized;
 
-                Handles.Label(position + (dir * output.distance * 0.5f), $"{output.distance:.02}", distanceLabel);
+                    Handles.color   = Color.magenta;
+                    Handles.DrawLine(position, target, 1.0f);
 
-                Handles.color   = Color.magenta;
-                Handles.DrawSolidDisc(target, Vector3.forward, 0.1f);
-                Handles.DrawLine(position, target, 1.0f);
-
-                Handles.Label(target, tg.application.tags.hash2Name(output.tag), this.targetLabel);
+                    if((this.target.sensorDetails & EnemyDebugging.SensorDetails.ShowDistance) != 0) { Handles.Label(position + (dir * output.distance * 0.5f), $"{output.distance:.02}", distanceLabel); }
+                    if((this.target.sensorDetails & EnemyDebugging.SensorDetails.ShowTargetTag) != 0) { Handles.Label(target, tg.application.tags.hash2Name(output.tag), this.targetLabel); }
+                }
             }
         }
     }
--- Assets/Runtime/tg/Modules/Debug/Enemy/EnemyDebugging.cs
+++ Assets/Runtime/tg/Modules/Debug/Enemy/EnemyDebugging.cs
@@ -6,34 +6,82 @@ using Unity.Entities;
 namespace tg.debug
 {
     using tg.ai;
+    using tg.ai.behaviour;
     using tg.enemy;
+    using static tg.level.generator.Generator;
 
     public class EnemyDebugging : MonoBehaviour
     {
-        public Enemy            self;
+        public Enemy                    self;
 
-        public Sensor           sensor { get; private set; }
+        private Coroutine               fetch;
 
-        private Coroutine       fetch;
+        public Sensor                   sensor { get; private set; } = Sensor.Default;
 
-        public void Start()
+        private BehaviourContextData[]  behaviourContextDataArray = new BehaviourContextData[BehaviourContextInternal.MAX_BEHAVIOURS];
+
+        public unsafe BehaviourSolver.BehaviourContextDataInternal     behaviourContextData
         {
-            this.fetch = this.StartCoroutine(this.fetchSensorData(World.DefaultGameObjectInjectionWorld.EntityManager, this.self));
+            get
+            {
+                var context = BehaviourContextData.Default;
+                switch(this.contextBehaviourDetails)
+                {
+                    case ContextBehaviourDetails.Wander:    { context = this.behaviourContextDataArray[IBehaviourContext<Wander>.ID]; break; }
+                    case ContextBehaviourDetails.Flee:      { context = this.behaviourContextDataArray[IBehaviourContext<Flee>.ID]; break; }
+                    case ContextBehaviourDetails.Avoid:     { context = this.behaviourContextDataArray[IBehaviourContext<Avoid>.ID]; break; }
+                    case ContextBehaviourDetails.Pursue:    { context = this.behaviourContextDataArray[IBehaviourContext<Pursue>.ID]; break; }
+                    case ContextBehaviourDetails.Solved:    { context = this.behaviourContextDataArray[IBehaviourContext<Solved>.ID]; break; }
+                }
+
+                return *(BehaviourSolver.BehaviourContextDataInternal*)&context;
+            }
         }
 
-        public void OnDestroy()
+        [System.Flags]
+        public enum SensorDetails
         {
-            this.StopCoroutine(this.fetch);
+            Hide            = 0,
+            ShowRange       = 1,
+            ShowOutput      = 2,
+            ShowDistance    = 4,
+            ShowTargetTag   = 8,
         }
+
+        public enum ContextBehaviourDetails
+        {
+            Hide,
+            Wander,
+            Flee,
+            Avoid,
+            Pursue,
+
+            Solved
+        }
+
+        public SensorDetails            sensorDetails           = SensorDetails.ShowRange | SensorDetails.ShowOutput | SensorDetails.ShowDistance | SensorDetails.ShowTargetTag;
+        public ContextBehaviourDetails  contextBehaviourDetails = ContextBehaviourDetails.Hide;
+
+        public void Start()     { this.fetch = this.StartCoroutine(this.fetchSensorData(World.DefaultGameObjectInjectionWorld.EntityManager, this.self)); }
+        public void OnDestroy() { this.StopCoroutine(this.fetch); }
 
         private IEnumerator fetchSensorData(EntityManager entityManager, Enemy self)
         {
             while(true)
             {
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(0.15f);
                 yield return new WaitForEndOfFrame();
 
-                this.sensor = entityManager.GetComponentData<Sensor>(self.entity);
+                if((this.sensorDetails & SensorDetails.Hide) == 0)
+                {
+                    this.sensor = entityManager.GetComponentData<Sensor>(self.entity);
+                }
+
+                if(this.contextBehaviourDetails != ContextBehaviourDetails.Hide)
+                {
+                    var buffer = entityManager.GetBuffer<BehaviourContextData>(self.entity, true);
+                    for(int i = 0; i < buffer.Length; i++) { this.behaviourContextDataArray[i] = buffer[i]; }
+                }
             }
         }
     }
