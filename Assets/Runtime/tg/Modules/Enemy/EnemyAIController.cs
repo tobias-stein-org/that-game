54b1a04af6c48ed85752b271caac7eb96f5555ae
Tobias S <>
2023-06-17 21:15:34 +0800

    Use most favorable context slot direction for AI steering

--- Assets/Runtime/tg/Modules/Enemy/EnemyAIController.cs
+++ Assets/Runtime/tg/Modules/Enemy/EnemyAIController.cs
@@ -18,6 +18,7 @@ namespace tg.enemy
         }
     }
 
+    [UpdateInGroup(typeof(LateSimulationSystemGroup))]
     [UpdateAfter(typeof(BehaviourSolver))]
     [ApplicationStateFilter(ApplicationStateMask.AllowRunWhenInGame)]
     [RequireMatchingQueriesForUpdate]
@@ -32,6 +33,21 @@ namespace tg.enemy
         {
             foreach(var (aiEnemy, rb) in SystemAPI.Query<AIEnemy, SystemAPI.ManagedAPI.UnityEngineComponent<Rigidbody2D>>())
             {
+                var context     = aiEnemy.solvedContext;
+                var min         = context[0];
+                var max         = context[0];
+                var imin        = 0;
+                var imax        = 0;
+
+                for(int i = 0; i < BehaviourContext.segmentDir.Length; i++)
+                {
+                    var value   = context[i];
+
+                    if(value > max) { max = value; imax = i; }
+                    if(value < min) { min = value; imin = i; }
+                }
+
+                rb.Value.velocity = BehaviourContext.segmentDir[imax];
             }
         }
     }
