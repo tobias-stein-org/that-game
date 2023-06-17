da48bbad7a75ac7b5dfe8949d9591ccd3af4eb46
Tobias S <>
2023-06-17 14:44:07 +0800

    add filterable enumerator for Sensor.Output

--- Assets/Runtime/tg/Core/AI/Sensor.cs
+++ Assets/Runtime/tg/Core/AI/Sensor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 using UnityEngine;
@@ -10,9 +12,10 @@ using SensorOutputBuffer = Unity.Collections.FixedList512Bytes<tg.ai.Sensor.Outp
 
 namespace tg.ai
 {
+    using System.Collections;
     using tg.application;
 
-    public struct Sensor : IComponentData, IEnableableComponent
+    public struct Sensor : IComponentData, IEnableableComponent, IEnumerable<Sensor.Output>
     {
         /// <summary>
         /// A description how a sensor should behave.
@@ -56,9 +59,135 @@ namespace tg.ai
         /// The output of a sensor
         /// </summary>
         [Serializable]
-        [StructLayout(LayoutKind.Sequential, Pack=1)]
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
         public struct Output
         {
+            /// <summary>
+            /// Enumerator class to iterate over sensor outputs.
+            /// </summary>
+            public class Enumerator : IEnumerator<Output>, IEnumerable<Output>
+            {
+                #region Filters
+
+                public interface IFilter
+                {
+                    bool check(in Output output);
+                }
+
+                private struct CombineFilter : IFilter
+                {
+                    public IFilter filter0;
+                    public IFilter filter1;
+
+                    public bool check(in Output output) { return this.filter0.check(in output) && this.filter1.check(in output); }
+                }
+
+                public struct FilterNone : IFilter
+                {
+                    public bool check(in Output output) { return true; }
+                }
+
+                public struct FilterWithTag : IFilter
+                {
+                    public TagHash      tag;
+
+                    public bool check(in Output output) { return output.tag == this.tag; }
+                }
+
+                public struct FilterWithTags : IFilter
+                {
+                    public TagHash[]    tags;
+
+                    public bool check(in Output output) { return this.tags.Contains(output.tag); }
+                }
+
+                public struct FilterWithInRange : IFilter
+                {
+                    public float minRange;
+                    public float maxRange;
+
+                    public bool check(in Output output) { return output.distance > this.minRange && output.distance < this.maxRange; }
+                }
+
+                #endregion
+
+                public Output          Current { get; private set; }
+                private int            index   = 0;
+
+
+                private IFilter        _filter  = new FilterNone();
+
+                private IFilter        filter
+                {
+                    get { return this._filter; }
+                    set
+                    {
+                        this._filter = this._filter is FilterNone ? value : new CombineFilter { filter0 = this._filter, filter1 = value };
+                    }
+                }
+                private readonly Sensor sensor;
+
+                public Enumerator(in Sensor sensor)
+                {
+                    this.sensor = sensor;
+                    this.index  = 0;
+                    this._filter = new FilterNone();
+                }
+
+                object IEnumerator.Current  => null;
+
+                public void Dispose() { }
+
+                public bool MoveNext()
+                {
+                    if(this.index < this.sensor.outputs.Length)
+                    {
+                        var output = this.sensor.outputs[this.index];
+                        this.index++;
+
+                        if(this.filter.check(in output))
+                        {
+                            this.Current = output;
+                            return true;
+                        }
+                        else
+                        {
+                            return this.MoveNext();
+                        }
+                    }
+
+                    this.Current = default;
+                    return false;
+                }
+
+                public void Reset() { this.index = 0; }
+
+                public IEnumerator<Output> GetEnumerator()  { return this; }
+                IEnumerator IEnumerable.GetEnumerator()     { return this; }
+
+                #region Queries
+
+                public Enumerator withTag(Tag tag)
+                {
+                    this.filter  = new FilterWithTag { tag = tag.hash };
+                    return this;
+                }
+
+                public Enumerator withTags(params Tag[] tags)
+                {
+                    this.filter  = new FilterWithTags { tags = tags.Select(tag => tag.hash).ToArray() };
+                    return this;
+                }
+
+                public Enumerator withInRange(float maxRange, float minRange = 0.0f)
+                {
+                    this.filter  = new FilterWithInRange { minRange = math.clamp(minRange, 0.0f, maxRange), maxRange = math.max(maxRange, 0.0f) };
+                    return this;
+                }
+
+                #endregion
+            }
+
             /// <summary>
             /// Point of collision.
             /// </summary>
@@ -117,6 +246,11 @@ namespace tg.ai
         }
 
         public static readonly Sensor   Default = new Sensor(Sensor.Description.Default);
+
+        public IEnumerator<Output> GetEnumerator()  { return new Output.Enumerator(this); }
+        IEnumerator IEnumerable.GetEnumerator()     { return this.GetEnumerator(); }
+
+        public Output.Enumerator  query()         { return new Output.Enumerator(this); }
     }
 }
 
