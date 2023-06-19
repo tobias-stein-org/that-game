f55317c2b4035ec13970aa31a6f2edb2db53b706
Tobias S <>
2023-06-19 13:56:40 +0800

    add some simple behaviour logic to AI controller

--- Assets/Runtime/tg/Modules/Enemy/EnemyAIController.cs
+++ Assets/Runtime/tg/Modules/Enemy/EnemyAIController.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using UnityEngine;
 using Unity.Entities;
 using Unity.Collections;
@@ -6,11 +8,14 @@ namespace tg.enemy
 {
     using tg.application;
     using tg.ai;
+    using tg.ai.behaviour;
 
     readonly partial struct AIEnemy : IAspect
     {
         readonly DynamicBuffer<BehaviourContextData>        behaviourContext;
         readonly RefRO<Enemy>                               enemy;
+
+        public  Entity                                      entity { get { return this.enemy.ValueRO.entity; } }
 
         public BehaviourSolver.BehaviourContextDataInternal solvedContext
         {
@@ -31,8 +36,30 @@ namespace tg.enemy
 
         public void OnUpdate(ref SystemState state)
         {
-            foreach(var (aiEnemy, rb) in SystemAPI.Query<AIEnemy, SystemAPI.ManagedAPI.UnityEngineComponent<Rigidbody2D>>())
+            foreach(var (aiEnemy, sensor, rb) in SystemAPI.Query<AIEnemy, Sensor, SystemAPI.ManagedAPI.UnityEngineComponent<Rigidbody2D>>())
             {
+                // TODO: at some point this logic has to be moved to a behviour tree
+                {
+                    var player  = sensor.query().withTag(tags.Player).FirstOrDefault();
+                    if(player.isValid && state.EntityManager.HasComponent<Pursue>(aiEnemy.entity))
+                    {
+                        var pursue          = state.EntityManager.GetComponentData<Pursue>(aiEnemy.entity);
+                        {
+                            pursue.target   = player.point;
+                        }
+                        state.EntityManager.SetComponentData<Pursue>(aiEnemy.entity,pursue);
+                        state.EntityManager.SetComponentEnabled<Pursue>(aiEnemy.entity, true);
+                        state.EntityManager.SetComponentEnabled<Wander>(aiEnemy.entity, false);
+                    }
+                    else
+                    {
+                        if(!state.EntityManager.IsComponentEnabled<Pursue>(aiEnemy.entity))
+                        {
+                            state.EntityManager.SetComponentEnabled<Wander>(aiEnemy.entity, true);
+                        }
+                    }
+                }
+
                 var contextData = aiEnemy.solvedContext;
                 var context0    = contextData.context;
                 var context1    = contextData.context1;
